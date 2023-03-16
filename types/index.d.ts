abd5a0d8764c1c46abd2c2c8a1e605516b029703
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-03-16 12:25:36 +0100

    v0.2.8
    Ts-errors in appConfig solved. Inserted this comment on line 1: // @ts-nocheck

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/guide/directory-structure/app-config
-declare module 'nuxt/schema' {
-  interface AppConfig {
-    // This will entirely replace the existing inferred `theme` property
-    theme: {
-      // You might want to type this value to add more specific types than Nuxt can infer,
-      // such as string literal types
-      primaryColor?: 'red' | 'blue'
-    }
+// https://nuxt.com/docs/guide/directory-structure/plugins#advanced
+declare module '#app' {
+  interface NuxtApp {
+    $hello (msg: string): string
   }
 }
 
-// It is always important to ensure you import/export something when augmenting a type
-export {}
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $hello (msg: string): string
+  }
+}
+
+export { }
