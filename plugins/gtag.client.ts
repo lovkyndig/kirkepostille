86ee8b5107a90b96b4606d215677067aa34ea85b
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-03-18 07:48:42 +0100

    v0.3.0-rc.7

--- plugins/gtag.client.ts
+++ plugins/gtag.client.ts
@@ -11,9 +11,12 @@ export default defineNuxtPlugin((nuxtApp) => {
   // }
 })
 /*
-source:
+sources:
 https://www.npmjs.com/package/vue-gtag
 https://matteo-gabriele.gitbook.io/vue-gtag/
 https://github.com/nuxt-community/google-analytics-module/issues/127
 https://github.com/nuxt/nuxt/discussions/17143
+https://stackoverflow.com/questions/71540309/how-to-connect-google-analytics-to-nuxt3-app
+https://github.com/nuxt-community/gtm-module/issues/152
+https://garywoodfine.com/how-to-add-google-analytics-to-nuxt-3/
 */
